d2f21f95d72f89ed42bdc3ca1cc9fd7e0d7e3136
pre14lim <s2s2hyun0601@gmail.com>
2022-10-10 23:44:46 +0900

    stack_programers

--- 13-programers_study/stack_index.js
+++ 13-programers_study/stack_index.js
@@ -19,11 +19,44 @@
 // 입출력 예 #1,2,3,4
 // 문제의 예시와 같습니다.
 
+// 스택을 이용해서 풀이해보면 스택이 빈배열이면 true 아니면 false 라고 생각을 해보고 풀이
+
+// for (const c of s) => for .. of 반복문을 통한 풀이
+// for .. of 반복문은 일반적으로 배열에 많이 사용되는데 , 배열의 요소 개수 만큼 반복하고 , 반복 때 마다 각 요소들을 사용할 수 있는 변수가
+// 주어지는 독특한 반복문이라고 할 수 있다.
+// // {
+//         #### 문법
+//         for( 변수 of 배열){
+//             반복문 동작 부분
+//         }
+// }
+
+// function solution(s) {
+//     const stack = [];
+//     for (const c of s) {
+//         if (c === "(") {
+//             stack.push(c);
+//         } else {
+//             if (stack.length === 0) {
+//                 return false;
+//             }
+//             stack.pop();
+//         }
+//     }
+//     return stack.length === 0;
+// }
+
 function solution(s) {
-    var answer = true;
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    console.log("Hello Javascript");
-
-    return answer;
+    let count = 0;
+    for (const c of s) {
+        if (c === "(") {
+            count += 1;
+        } else {
+            if (count === 0) {
+                return false;
+            }
+            count -= 1;
+        }
+    }
+    return count === 0;
 }
